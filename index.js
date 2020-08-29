6775ae5ad903400cdcb14e77832321a17041ff8c
junko911 <junkotahara911@gmail.com>
2020-08-28 23:09:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+const findMatching = (array, name) => {
+  const lower = name[0].toLowerCase() + name.slice(1)
+  const upper = name[0].toUpperCase() + name.slice(1)
+  return array.filter(e => e === lower || e === upper)
+}
+
+const fuzzyMatch = (array, letter) => {
+  const lower = letter[0].toLowerCase()
+  const upper = letter[0].toUpperCase()
+  return array.filter(e => e[0] === lower || e[0] === upper)
+}
+
+const matchName = (obj, name) => {
+  return obj.filter(e => e.name === name)
+}
