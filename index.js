c40e29426bcee898c6ac683d7b884b4270c30374
github username <joshmrallen@gmail.com>
2020-08-29 15:08:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+// function findMatching(names, search){
+//     return names.filter(name => (name === search) || (name === (search.charAt(0).toUpperCase() + search.slice(1))))
+// }
+// //wtf
+
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+
+
+// function fuzzyMatch(names, search) {
+//     return names.filter(function(name){
+//         for(const searchLetter of search){
+//             for(const nameLetter of name){
+//                 searchLetter === nameLetter
+//             }
+//         }
+//     })
+// }
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+
+function matchName(source, string){
+    return source.filter(possibleMatch => 
+        possibleMatch.name.toLowerCase() === string.toLowerCase()    
+    )
+}
