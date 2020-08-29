f4f96a81030a400191a22114acd3cb2c13d6b277
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-29 14:38:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {return driver.toLowerCase() == string.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, string) {
+    let number = string.length
+    return drivers.filter(function(driver) {return driver.substr(0, number) == string})
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {return driver.name == string})
+}
